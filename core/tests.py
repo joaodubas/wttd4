5e11ee65098cd382e78b42c1258c51d89eeec4e7
Joao P Dubas <joao.dubas@gmail.com>
2011-08-21 20:26:16 -0300

    added test to index page

--- core/tests.py
+++ core/tests.py
@@ -1,16 +1,11 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class HomepageUrlTest(TestCase):
+    def test_success_when_get_homepage(self):
+        '''
+        Verifies if the homepage is loaded and uses the specified template.
+        '''
+        response = self.client.get('/')
+        self.assertEquals(200, response.status_code)
+        self.assertTemplateUsed(response, 'index.html')
