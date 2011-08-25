c223eb2081a3845dd714752daff6002d2b7ebd5b
Joao P Dubas <joao.dubas@gmail.com>
2011-08-24 22:31:22 -0300

    removed unused code

--- core/tests.py
+++ core/tests.py
@@ -1,11 +1,1 @@
 from django.test import TestCase
-
-
-class HomepageUrlTest(TestCase):
-    def test_success_when_get_homepage(self):
-        '''
-        Verifies if the homepage is loaded and uses the specified template.
-        '''
-        response = self.client.get('/')
-        self.assertEquals(200, response.status_code)
-        self.assertTemplateUsed(response, 'index.html')
