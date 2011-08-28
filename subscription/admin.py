e103a39c4404ea0b9f0db90a050ffd73dd7acd5f
Joao P Dubas <joao.dubas@gmail.com>
2011-08-28 10:31:27 -0300

    started using il8n on subscription

--- subscription/admin.py
+++ subscription/admin.py
@@ -1,5 +1,6 @@
 from datetime import date
 from django.contrib import admin
+from django.utils.translation import ugettext as _
 from subscription.models import Subscription
 
 class SubscriptionAdmin(admin.ModelAdmin):
@@ -7,11 +8,12 @@ class SubscriptionAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_at'
     search_fields = ('name', 'cpf', 'email', 'phone', 'created_at', )
     list_filter = ('created_at', )
+    actions = ['mark_as_paid']
 
     def today(self, obj):
         return date.today() == obj.created_at.date()
     today.boolean = True
-    today.short_description = u'Inscrito hoje?'
+    today.short_description = _(u'Inscrito hoje?')
 
 
 admin.site.register(Subscription, SubscriptionAdmin)
--- subscription/models.py
+++ subscription/models.py
@@ -1,20 +1,21 @@
 # -*- encoding: utf-8 -*-
+from django.utils.translation import ugettext as _
 from django.db import models
 from subscription.signals import email_success_subscription
 
 class Subscription(models.Model):
-    name = models.CharField(max_length=128, verbose_name='Nome')
-    email = models.EmailField(unique=True, verbose_name='E-mail')
-    cpf = models.CharField(max_length=11, unique=True, verbose_name='CPF')
-    phone = models.CharField(max_length=10, blank=True, verbose_name='Telefone')
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Criado em')
+    name = models.CharField(max_length=128, verbose_name=_('Nome'))
+    email = models.EmailField(unique=True, verbose_name=_('E-mail'))
+    cpf = models.CharField(max_length=11, unique=True, verbose_name=_('CPF'))
+    phone = models.CharField(max_length=10, blank=True, verbose_name=_('Telefone'))
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name=_('Criado em'))
 
     class Meta:
         ordering = ('-created_at', )
-        verbose_name = u'Inscrição'
-        verbose_name_plural = u'Inscrições'
+        verbose_name = _(u'Inscrição')
+        verbose_name_plural = _(u'Inscrições')
     
     def __unicode__(self):
-        return '%(name)s <%(email)s> inscrito em %(created_at)s' % self.__dict__
+        return _(u'%(name)s <%(email)s> inscrito em %(created_at)s') % self.__dict__
 models.signals.post_save.connect(email_success_subscription, Subscription)
 
